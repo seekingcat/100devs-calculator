feff810daf2d7cd2e0d4cc34ddcfbe238de4ce71
seekingcat <llana_a154@yahoo.co.uk>
2024-10-01 08:22:31 +0100

    oop code that does not yet work

--- js/main.js
+++ js/main.js
@@ -6,16 +6,50 @@ let clear = document.querySelector('#clear');
 
 calcButton.addEventListener('click', function (event) {
   let target = event.target;
-  if (target.className != 'calcButton') return;
+  if (target.className != 'calcButton' || target.idName == 'equal') return;
   answer.innerText += target.innerText;
   equal.addEventListener('click', function () {
-    answer.innerText = eval(answer.innerText);
+    answer.innerText = eval(answer.innerText).toFixed(8);
+  });
+  clear.addEventListener('click', function () {
+    answer.innerText = '';
   });
 });
 
 multiply.addEventListener('click', function () {
   answer.innerText += '*';
 });
-clear.addEventListener('click', function () {
-  answer.innerText = '';
-});
+
+//  make the below functionality work
+
+// class Calculator {
+//   constructor() {
+//     this.answer = document.querySelector('.answerBox');
+//     this.equal = document.querySelector('#equal');
+//     this.multiply = document.querySelector('#multiply');
+//     this.calcButton = document.querySelector('.numberPlate');
+//     this.clear = document.querySelector('#clear');
+//     this.calculate();
+//   }
+
+//   calculate() {
+//     this.calcButton.addEventListener('click', (event) => this.doCalc(event));
+//   }
+
+//   doCalc() {
+//     let target = event.target;
+//     if (target.className != 'calcButton' || target.idName == 'equal') return;
+//     this.answer.innerText += target.innerText;
+//     this.equal.addEventListener('click', function () {
+//       this.answer.innerText = eval(this.answer.innerText).toFixed(8);
+//     });
+//     this.clear.addEventListener('click', function () {
+//       this.answer.innerText = '';
+//     });
+
+//     this.multiply.addEventListener('click', function () {
+//       this.answer.innerText += '*';
+//     });
+//   }
+// }
+// document.addEventListener('DOMContentLoaded', () => new Calculator());
